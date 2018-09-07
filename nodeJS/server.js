d33224b9689c7e0d35bed7f89584199dcd03da7e
evan <ekinmomo@gmail.com>
2018-09-07 18:02:47 +0800

    nodejs練習

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,14 +1,24 @@
-const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Nodejs\n');
-});
+const http = require("http");
+const url = require("url");
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for" + pathname + "received.");
+        
+        
+
+        response.writeHead(200, {"Content-type": "text/plain"});
+        var content = route(handle, pathname);
+        response.write(content);
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+}
+
+exports.start = start;
+
+
